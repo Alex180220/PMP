c671cffd4e101a6fa9bfa1f219da1510b244bb32
Alex180220 <alexfurtuna95@gmail.com>
2022-09-30 13:34:50 +0300

    Laborator 1

--- Lab1/hello_world.py
+++ Lab1/hello_world.py
@@ -5,47 +5,16 @@ import numpy as np
 
 import pymc3 as pm
 
+mu = 0
+sigma = 0.05
+s = np.random.normal(mu, sigma, 2000)
 
-# Generating data
-# True parameter values
-alpha, sigma = 1, 1
-beta = [1, 2.5]
+abs(mu - np.mean(s))
 
-# Size of dataset
-size = 100
+abs(sigma - np.std(s, ddof=1))
 
-# Predictor variable
-X1 = np.random.randn(size)
-X2 = np.random.randn(size) * 0.2
-
-# Simulate outcome variable
-Y = alpha + beta[0] * X1 + beta[1] * X2 + np.random.randn(size) * sigma
-
-# Vizualize the data
-fig, axes = plt.subplots(1, 2, sharex=True, figsize=(10, 4))
-axes[0].scatter(X1, Y, alpha=0.6)
-axes[1].scatter(X2, Y, alpha=0.6)
-axes[0].set_ylabel("Y")
-axes[0].set_xlabel("X1")
-axes[1].set_xlabel("X2")
+count, bins, ignored = plt.hist(s, 100, density=True)
+plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *
+               np.exp( - (bins - mu)**2 / (2 * sigma**2) ),
+         linewidth=2)
 plt.show()
-
-# Define a model
-basic_model = pm.Model()
-
-with basic_model:
-
-    # Priors for unknown model parameters
-    alpha = pm.Normal("alpha", mu=0, sigma=10)
-    beta = pm.Normal("beta", mu=0, sigma=10, shape=2)
-    sigma = pm.HalfNormal("sigma", sigma=1)
-
-    # Expected value of outcome
-    mu = alpha + beta[0] * X1 + beta[1] * X2
-
-    # Likelihood (sampling distribution) of observations
-    Y_obs = pm.Normal("Y_obs", mu=mu, sigma=sigma, observed=Y)
-
-#Fit model to data and print estimates
-map_estimate = pm.find_MAP(model=basic_model)
-print(map_estimate)
